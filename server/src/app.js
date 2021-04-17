b17cf81a2d521209b4262bd2b1e969f53694996b
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-04-17 18:26:25 +0530

    connect mongodb atlas

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,27 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
+import mongoose from 'mongoose'
 
 // EXPRESS APP
 const app = express()
 
 // CONSTANTS
 const PORT = process.env.PORT || 5000
+
+// CONNECTING MONGODB ATLAS 
+console.log("SB Name "+ process.env.DB_NAME)
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@akshay.vlzrm.mongodb.net/${process.env.DB_NAME}`
+const options = {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+}
+mongoose.connect(url, options).then(() => {
+    console.log('Connected To MongoDB Atlas !')
+}).catch(error => {
+    console.log(error)
+})
 
 // HOME ROUTE
 app.get('/', (req, res) => {
